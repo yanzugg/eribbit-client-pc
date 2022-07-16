bc72564ed802718c05651220f42adab4a9eb5d36
yanzugg <790042082@qq.com>
2022-07-16 18:28:16 +0800

    初始化了项目

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,29 @@
 import { createStore } from 'vuex'
 
+// vue2.0 创建仓库 new Vuex.store({})
+// vue3.0 创建仓库 createStore({})
 export default createStore({
+
   state: {
+    username: 'zs'
   },
   getters: {
+    newName (state) {
+      return state.username + '!!!'
+    }
   },
   mutations: {
+    updateName (state) {
+      state.username = 'ls'
+    }
   },
   actions: {
+    updateName (context) {
+    //  发请求
+      setTimeout(() => {
+        context.commit('updateName')
+      }, 1000)
+    }
   },
   modules: {
   }
